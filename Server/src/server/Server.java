aefde37ac072137d9147392ea68e12b004c73e7e
AitorZaragoza <aitorzag@gmail.com>
2020-11-21 19:41:37 +0100

    Corrected CSV file path

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -13,34 +13,36 @@ public class Server {
 
     public static void readCsv() {
 
-        String csvFile = "../ExamQuestions.csv";
+        String csvFile = "ExamQuestions.csv";
         BufferedReader br = null;
         String line;
-        List<String> questions = new ArrayList<String>();
-        List<String> correct_answer = new ArrayList<String>();
+        List<String> questions = new ArrayList<>();
+        List<String> correct_answer = new ArrayList<>();
         List<String> temporal_answer;
-        List<List<String>> answers = new ArrayList<List<String>>();
+        List<List<String>> answers = new ArrayList<>();
 
 
         try {
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
 
-                temporal_answer = new ArrayList<>();
+
                 String[] data = line.split(";");
+                int size = data.length;
+                temporal_answer = new ArrayList<>(size - 2);
 
                 questions.add(data[0]);
                 correct_answer.add(data[data.length - 1]);
 
-                for (int i = 1; i < data.length - 1; i++){
+                for (int i = 1; i < size - 1; i++){
                     temporal_answer.add(data[i]);
                 }
                 answers.add(temporal_answer);
 
             }
-            /*System.out.println(questions.get(1));
+            System.out.println(questions.get(1));
             System.out.println(answers.get(1));
-            System.out.println(correct_answer.get(1));*/
+            System.out.println(correct_answer.get(1));
 
 
         } catch (FileNotFoundException e) {
